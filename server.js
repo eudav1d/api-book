b9f70f77d98d4a05c993746732347c03c0ab96e2
eudav1d <davidnunes520@gmail.com>
2026-09-21 20:25:39 -0300

    finalizando hoje

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+app.set('json spaces', 2); 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('E aí bb, tudo certo?')
 })
 
 //endpoint para rota /turma
@@ -11,6 +13,36 @@ app.get('/turma', (req, res) => {
     res.send('Bem-vindo à rota /turma!')
 })
 
+//endpoint para rota /books
+app.get('/books', (req, res) => {
+    res.json([
+    {
+        id: 1,
+        title: 'O Senhor dos Anéis',
+        author: 'J.R.R. Tolkiin'
+    },
+    {
+        id: 2,
+        title: 'As branquelas',
+        author: ['Keenen Ivory Wayans', 'Marlon Wayans', 'Shawn Wayans']
+    },
+    {
+        id: 3,
+        title: 'Toy Story',
+        author: 'John Lasseter'
+    }
+])
+})
+
+app.get('/books/:id', (req, res) => {
+    const bookId = parseInt(req.params.id);
+    const book = books.find(b => b.id === bookId);
+    if (!book) {
+        return res.status(404).json({ error: 'Livro não encontrado' });
+    }
+    res.json(book);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`API-BOOK rodando na porta ${port}`)
 })
